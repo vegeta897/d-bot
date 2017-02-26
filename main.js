8df09574b46ca65bae5f0a63d13acd838d6de681
Devin Spikowski <vegeta897@gmail.com>
2017-02-26 01:39:05 -0500

    Add discord-spoiler-bot

--- main.js
+++ main.js
@@ -7,10 +7,17 @@ var addons = require('./core/addons.js');
 
 // TODO: Delete original command and error message when a command is retried successfully immediately after
 
+var SpoilerBot = require('discord-spoiler-bot');
+var spoilBot = new SpoilerBot({
+    client: discord.bot,
+    markAllowAll: true
+});
+
 discord.bot.on('ready', function(event) {
     console.log((new Date()).toString().substr(0,24),
         `Logged in as: ${discord.bot.username} - (${discord.bot.id})`);
     require('fs').writeFile('./debug/bot.json', JSON.stringify(discord.bot, null, '\t'));
+    spoilBot.connect();
 });
 
 discord.bot.on('message', function(user, userID, channelID, message, rawEvent) {
