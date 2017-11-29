7296d764b5c52f5d107b974e99f7dca080544fed
vegeta897 <vegeta897@gmail.com>
2017-11-29 09:20:18 -0500

    Add slot selection and player avatar ball to pachinko

--- addons/pachinko.js
+++ addons/pachinko.js
@@ -8,6 +8,7 @@ const GIFEncoder = require('gifencoder');
 const fs = require('fs');
 const { execFile } = require('child_process');
 const gifsicle = require('gifsicle');
+const download = require('download');
 
 const _commands = {};
 
@@ -18,6 +19,8 @@ const WIDTH = 200;
 const HEIGHT = 300;
 const TOP_SPACE = 40; // Before pegs
 const BOTTOM_SPACE = 50; // After pegs
+const SLOTS = 6; // Deploy positions
+const SLOT_WIDTH = WIDTH / (SLOTS + 1);
 const GOALS = 8;
 const GOAL_HEIGHT = 20;
 const PEG_RADIUS = 3;
@@ -37,6 +40,30 @@ const FPS = 30;
 const BORDER_COLOR = '#AAAAAA';
 const PEG_COLOR = '#CCCCCC';
 const BG_COLOR = '#202225';
+
+const SLOT_IMAGE = new Canvas(WIDTH * RES, TOP_SPACE * RES);
+
+function drawSlots() {
+    let slotCtx = SLOT_IMAGE.getContext('2d');
+    slotCtx.strokeStyle = BORDER_COLOR;
+    slotCtx.lineWidth = RES;
+    slotCtx.fillStyle = PEG_COLOR;
+    slotCtx.font = `${Math.floor(TOP_SPACE / 2.8 * RES)}px Roboto`;
+    slotCtx.textBaseline = 'top';
+    slotCtx.textAlign = 'center';
+    for(let s = 0; s < SLOTS; s++) {
+        let x = Math.floor((s + 1) * SLOT_WIDTH * RES);
+        slotCtx.beginPath();
+        slotCtx.moveTo(x + 0.5, TOP_SPACE / 2 * RES);
+        slotCtx.lineTo(x + 0.5, TOP_SPACE * 0.75 * RES);
+        slotCtx.moveTo(x + 0.5 - SLOT_WIDTH / 10 * RES, TOP_SPACE * 0.7 * RES);
+        slotCtx.lineTo(x + 0.5, TOP_SPACE * 0.75 * RES);
+        slotCtx.lineTo(x + 0.5 + SLOT_WIDTH / 10 * RES, TOP_SPACE * 0.7 * RES);
+        slotCtx.stroke();
+        slotCtx.fillText((s + 1).toString(), x + RES - (s + 1 === 1 ? 1 : 0) * RES, 0);
+    }
+}
+drawSlots();
 
 let game = false;
 
@@ -74,7 +101,7 @@ function drawMap(map) {
     ctx.fillStyle = PEG_COLOR;
     for(let [pegX, pegY] of map.pegs) {
         ctx.beginPath();
-        ctx.arc(pegX * RES, pegY * RES, PEG_RADIUS * RES, 0, 2 * Math.PI, false);
+        ctx.arc(pegX * RES, pegY * RES, PEG_RADIUS * RES, 0, 2 * Math.PI);
         ctx.fill();
     }
     return canvas;
@@ -94,9 +121,11 @@ function simulate(map, cb) {
     encoder.setRepeat(0);
     encoder.setDelay(Math.round(1000 / FPS));
     encoder.setQuality(5);
-    let x = WIDTH / 2;
+    let player = game.players.get('86913608335773696');
+    let ballImg = player.avatarImg;
+    let x = player.slot * SLOT_WIDTH;
     let y = BALL_RADIUS;
-    let v = { x: 0.7, y: 0 };
+    let v = { x: util.random(-0.2, 0.2), y: 0 };
     ctx.fillStyle = '#7E89DE';
     //console.time('simulating result');
     //util.timer.reset();
@@ -104,10 +133,12 @@ function simulate(map, cb) {
     while(y < HEIGHT - BALL_RADIUS) {
         //util.timer.start('drawing frame');
         let subFrame = frame % FRAME_DIVIDER;
-        ctx.beginPath();
         ctx.globalAlpha = subFrame === 0 ? 1 : Math.pow(0.5, FRAME_DIVIDER - subFrame);
-        ctx.arc(x * RES, y * RES, BALL_RADIUS * RES, 0, 2 * Math.PI, false);
-        ctx.fill();
+        ctx.save();
+        ctx.shadowColor = '#7E89DE';
+        ctx.shadowBlur = 5 * RES;
+        ctx.drawImage(ballImg, (x - BALL_RADIUS) * RES, (y - BALL_RADIUS) * RES);
+        ctx.restore();
         if(subFrame === 0) {
             encoder.addFrame(ctx);
             ctx.drawImage(map.image, 0, 0);
@@ -151,27 +182,53 @@ function simulate(map, cb) {
 _commands.pachinko = function(data) {
     game = false;
     if(game) return data.reply('There is already a pachinko game in progress!');
-    game = { players: [] };
+    game = { players: new Map() };
     game.channel = data.channel;
     game.map = generateMap();
     game.map.image = drawMap(game.map);
+    let canvas = new Canvas(game.map.image.width, game.map.image.height);
+    let ctx = canvas.getContext('2d');
+    ctx.drawImage(game.map.image, 0, 0);
+    ctx.drawImage(SLOT_IMAGE, 0, 0);
     discord.bot.uploadFile({
-        to: data.channel, filename: `pachinko-${Date.now()}.png`, file: game.map.image.toBuffer()
+        to: data.channel, filename: `pachinko-${Date.now()}.png`, file: canvas.toBuffer(),
+        message: `**__Pachinko!__**\nUse \`${config.prefixes[0]}p\` to choose a slot #`
     });
-    setTimeout(() => {
-        simulate(game.map, (err, file) => {
-            if(err) return console.log(err);
-            discord.bot.uploadFile({
-                to: data.channel, filename: `pachinko-${Date.now()}.gif`, file
-            });
-        });
-    }, 1000)
 };
 
 module.exports = {
     listen(data) {
-        if(!game || !game.channel !== data.channel) return;
-        
+        if(!game || game.channel !== data.channel || data.command !== 'p') return;
+        let slot = +data.paramStr;
+        if(!(slot > 0 && slot <= SLOTS)) return data.reply(`Pick a slot from 1 to ${SLOTS}`);
+        game.players.set(data.userID, {
+            slot
+        });
+        let avatarURL = `https://cdn.discordapp.com/avatars/${data.userID}/${discord.bot.users[data.userID].avatar}.png`;
+        download(avatarURL).then(imgData => {
+            let img = new Canvas.Image;
+            img.src = imgData;
+            let avatarCanvas = new Canvas(BALL_RADIUS * 2 * RES, BALL_RADIUS * 2 * RES);
+            let avatarCtx = avatarCanvas.getContext('2d');
+            avatarCtx.beginPath();
+            avatarCtx.arc(BALL_RADIUS * RES, BALL_RADIUS * RES, BALL_RADIUS * RES, 0, 2 * Math.PI);
+            avatarCtx.clip();
+            avatarCtx.drawImage(
+                img, img.width / 5, img.width / 5, img.width * 3 / 5, img.height / 2, 
+                0, 0, avatarCanvas.width, avatarCanvas.height
+            );
+            game.players.get(data.userID).avatarImg = avatarCanvas;
+            simulate(game.map, (err, file) => {
+                if(err) return console.log(err);
+                discord.bot.uploadFile({
+                    to: data.channel, filename: `pachinko-${Date.now()}.gif`, file
+                });
+            });
+        }).catch(err => {
+            console.log(err);
+            game.players.get(data.userID).avatarImg = false;
+        });
     },
+    dev: true,
     commands: _commands
 };
