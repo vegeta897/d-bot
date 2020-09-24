711c9bbb1f85263e96da8c480b7429643f86a6d6
Devin Spikowski <vegeta897@gmail.com>
2020-09-24 11:27:29 -0400

    Restructure Discord static class, add findUserByName method

--- src/Core/Discord.ts
+++ src/Core/Discord.ts
@@ -1,22 +1,44 @@
-import { CommandClient } from 'eris'
+import { CommandClient, User } from 'eris'
 import { DISCORD_TOKEN } from '../config'
 
-export class Discord extends CommandClient {
-	private static instance: Discord
-	private constructor() {
-		super(
-			DISCORD_TOKEN,
-			{},
-			{
-				prefix: ']',
-				owner: 'vegeta897#7777',
-			}
-		)
-	}
-	static get bot(): Discord {
-		if (!this.instance) {
-			this.instance = new Discord()
+export class Discord {
+	private static clientInstance: CommandClient
+	static get bot(): CommandClient {
+		if (!this.clientInstance) {
+			this.clientInstance = new CommandClient(
+				DISCORD_TOKEN,
+				{},
+				{
+					prefix: ']',
+					owner: 'vegeta897#7777',
+				}
+			)
 		}
-		return this.instance
+		return this.clientInstance
 	}
+	static findUserByName(
+		name: string,
+		{
+			ignoreNick = false,
+			matchCase = false,
+		}: { ignoreNick?: boolean; matchCase?: boolean } = {}
+	): User | undefined {
+		const foundUser = this.bot.users.find((u) =>
+			compareStrings(u.username, name, matchCase)
+		)
+		if (foundUser || ignoreNick) return foundUser
+		// Search nicknames if username not found
+		for (const [, guild] of this.bot.guilds) {
+			const foundMember = guild.members.find(
+				(m) => !!m.nick && compareStrings(m.nick, name, matchCase)
+			)
+			if (foundMember) return this.bot.users.get(foundMember.id)
+		}
+		return undefined
+	}
+}
+
+function compareStrings(a: string, b: string, matchCase: boolean): boolean {
+	if (matchCase) return a === b
+	else return a.toLowerCase() === b.toLowerCase()
 }
