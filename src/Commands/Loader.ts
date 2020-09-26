0dad1596a1e14ae08acc93d31ef90c0593d17d16
Devin Spikowski <vegeta897@gmail.com>
2020-09-26 16:36:02 -0400

    Load any DBotCommand exports of command modules

--- src/Commands/Loader.ts
+++ src/Commands/Loader.ts
@@ -1,5 +1,6 @@
 import glob from 'glob'
 import { Discord } from '../Core/Discord'
+import { DBotCommand } from './Command'
 
 export async function LoadCommands(): Promise<void> {
 	glob('*/Commands/*/*Command.ts', async (err, matches) => {
@@ -10,9 +11,15 @@ export async function LoadCommands(): Promise<void> {
 				const [, commandName] = match.match(
 					/^src\/Commands\/\w+\/(\w+)Command\.ts$/
 				) as string[]
-				const command = await import(`./${commandName}/${commandName}Command`)
-				command[commandName + 'Command'].register(Discord.bot)
-				loaded++
+				const commandModule = await import(
+					`./${commandName}/${commandName}Command`
+				)
+				for (const exportName of Object.keys(commandModule)) {
+					if (commandModule[exportName] instanceof DBotCommand) {
+						commandModule[exportName].register(Discord.bot)
+						loaded++
+					}
+				}
 			} catch (e) {
 				console.error(e)
 			}
