3a0b813ba4d0289d901852ea67e77753a4147ea5
Devin Spikowski <vegeta897@gmail.com>
2020-10-29 06:47:22 -0400

    Increase coin flip limit with totals for truncated results

--- src/Commands/Flip/FlipCommand.ts
+++ src/Commands/Flip/FlipCommand.ts
@@ -9,8 +9,8 @@ export const FlipCommand = new DBotCommand({
 		if (params.length > 0) {
 			flips = +params[0]
 			if (isNaN(flips)) flips = 1
-			if (flips > 40) throw 'Number of flips must be less than 40'
 			if (flips < 1) throw 'Number of flips must be at least 1'
+			if (flips > 1000000) throw 'Number of flips must be less than a million'
 		}
 		const message = [
 			`Flipping a coin${flips > 1 ? ` **${flips}** times` : ''}!`,
@@ -19,10 +19,16 @@ export const FlipCommand = new DBotCommand({
 		for (let i = 0; i < flips; i++) {
 			results.push(`**${random.int() > 0 ? 'Heads' : 'Tails'}**`)
 		}
-		const { resultList } = CreateResultList(results, {
+		const { resultList, truncated } = CreateResultList(results, {
 			resultsPerLineSizes: [1, 5, 7],
 		})
 		message.push(resultList)
+		if (truncated) {
+			const headsCount = results.filter((r) => r.includes('Heads')).length
+			message.push(
+				`Heads: **${headsCount}**, Tails: **${results.length - headsCount}**`
+			)
+		}
 		return message.join('\n')
 	},
 	commandOptions: {
--- tests/Commands/Flip/FlipCommand.test.ts
+++ tests/Commands/Flip/FlipCommand.test.ts
@@ -6,8 +6,10 @@ describe('flip command execution', () => {
 	it('returns a string with expected line count', () => {
 		const singleFlip = execute({ params: ['1'] }) as string
 		const multiFlip = execute({ params: ['5'] }) as string
+		const multiFlipTruncated = execute({ params: ['100'] }) as string
 		expect(singleFlip.split('\n')).toHaveLength(2)
-		expect(multiFlip.split('\n').length).toBeGreaterThan(2)
+		expect(multiFlip.split('\n')).toHaveLength(6)
+		expect(multiFlipTruncated.split('\n')).toHaveLength(4)
 	})
 	it('treats non-numeric flip count as 1', () => {
 		const noFlip = execute({ params: [] }) as string
@@ -16,7 +18,7 @@ describe('flip command execution', () => {
 		expect(aFlip.split('\n')).toHaveLength(2)
 	})
 	it('throws with invalid parameters', () => {
-		expect(() => execute({ params: ['999'] })).toThrow(
+		expect(() => execute({ params: ['9999999'] })).toThrow(
 			'flips must be less than'
 		)
 		expect(() => execute({ params: ['0'] })).toThrow('flips must be at least 1')
