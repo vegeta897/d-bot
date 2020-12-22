d533bc77a796568af91b34ab6775d3658b063710
Devin Spikowski <vegeta897@gmail.com>
2020-12-21 19:59:36 -0500

    Update superstruct and fix Parser typing

--- src/Commands/Parser.ts
+++ src/Commands/Parser.ts
@@ -1,18 +1,18 @@
-import { is, Struct } from 'superstruct'
+import { Infer, is, Struct } from 'superstruct'
 
-interface ParserOptions<T extends Record<string, unknown>> {
-	parsers: { (params: string[]): T | void }[]
-	validator: Struct<T>
+interface ParserOptions<T extends Struct<Infer<T>>> {
+	parsers: { (params: string[]): Infer<T> | void }[]
+	validator: T
 }
 
-export class Parser<T extends Record<string, unknown>> {
+export class Parser<T extends Struct<Infer<T>>> {
 	private readonly parsers
 	private readonly validator
 	constructor({ parsers, validator }: ParserOptions<T>) {
 		this.parsers = parsers
 		this.validator = validator
 	}
-	parse = (params: string[]): T => {
+	parse = (params: string[]): Infer<T> => {
 		let parsed
 		for (const parser of this.parsers) {
 			const parseAttempt = parser(params)
