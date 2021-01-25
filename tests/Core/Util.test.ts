bd32027353c6b7ed97e9185354e3b03e5e7ffbe5
Devin Spikowski <vegeta897@gmail.com>
2021-01-25 08:32:06 -0500

    :white_check_mark: Simplify value type check tests

--- tests/Core/Util.test.ts
+++ tests/Core/Util.test.ts
@@ -19,67 +19,30 @@ const otherValueTypes = [
 	new Date(),
 	/./,
 ]
-const allTypes = [
-	...objectTypes,
-	...mapTypes,
-	...arrayTypes,
-	...numberTypes,
-	...stringTypes,
-	...otherValueTypes,
-]
 
-describe('isObject', () => {
-	it('returns true for objects', () => {
-		objectTypes.forEach((v) => expect(isObject(v)).toBeTruthy())
-	})
-	it('returns false for non-objects', () => {
-		const nonObjectTypes = allTypes.filter(
-			(type) => !objectTypes.includes(type)
-		)
-		nonObjectTypes.forEach((v) => expect(isObject(v)).toBeFalsy())
-	})
-})
-
-describe('isMap', () => {
-	it('returns true for maps', () => {
-		mapTypes.forEach((v) => expect(isMap(v)).toBeTruthy())
-	})
-	it('returns false for non-maps', () => {
-		const nonMapTypes = allTypes.filter((type) => !mapTypes.includes(type))
-		nonMapTypes.forEach((v) => expect(isMap(v)).toBeFalsy())
-	})
-})
-
-describe('isArray', () => {
-	it('returns true for arrays', () => {
-		arrayTypes.forEach((v) => expect(isArray(v)).toBeTruthy())
-	})
-	it('returns false for non-arrays', () => {
-		const nonArrayTypes = allTypes.filter((type) => !arrayTypes.includes(type))
-		nonArrayTypes.forEach((v) => expect(isArray(v)).toBeFalsy())
-	})
-})
-
-describe('isString', () => {
-	it('returns true for strings', () => {
-		stringTypes.forEach((v) => expect(isString(v)).toBeTruthy())
-	})
-	it('returns false for non-strings', () => {
-		const nonStringTypes = allTypes.filter(
-			(type) => !stringTypes.includes(type)
-		)
-		nonStringTypes.forEach((v) => expect(isString(v)).toBeFalsy())
-	})
-})
-
-describe('isNumber', () => {
-	it('returns true for numbers', () => {
-		numberTypes.forEach((v) => expect(isNumber(v)).toBeTruthy())
-	})
-	it('returns false for non-numbers', () => {
-		const nonNumberTypes = allTypes.filter(
-			(type) => !numberTypes.includes(type)
-		)
-		nonNumberTypes.forEach((v) => expect(isNumber(v)).toBeFalsy())
+describe('value type check functions', () => {
+	const tests: Map<string, [unknown[], (v: unknown) => boolean]> = new Map([
+		['objects', [objectTypes, isObject]],
+		['maps', [mapTypes, isMap]],
+		['arrays', [arrayTypes, isArray]],
+		['numbers', [numberTypes, isNumber]],
+		['strings', [stringTypes, isString]],
+	])
+	tests.forEach(([types, is], name) => {
+		it(`returns true for ${name}`, () => {
+			types.forEach((v) => expect(is(v)).toBeTruthy())
+		})
+		it(`returns false for non-${name}`, () => {
+			;[
+				...objectTypes,
+				...mapTypes,
+				...arrayTypes,
+				...numberTypes,
+				...stringTypes,
+				...otherValueTypes,
+			]
+				.filter((type) => !types.includes(type))
+				.forEach((v) => expect(is(v)).toBeFalsy())
+		})
 	})
 })
